2ddc7a347bd2dd5321193ff729b64ab4ef240b72
FILOSOFER01 <emmanuelokeowo14@gmail.com>
2025-07-03 16:53:14 -0700

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   const toggle = document.getElementById("menu-toggle");
+//   const menu = document.getElementById("nav-menu");
+
+//   if (toggle && menu) {
+//     toggle.addEventListener("click", () => {
+//       menu.classList.toggle("active");
+//       console.log("Toggle clicked!"); // Check if this logs
+//     });
+//   } else {
+//     console.error("Menu toggle or nav menu not found.");
+//   }
+// });
+
+
+
+
